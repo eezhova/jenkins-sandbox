aa5608b9f647297e64f59f3b59711b04ac60faf7
Elena Ezhova <eezhova@mirantis.com>
2017-11-30 15:50:26 +0400

    Don't try to send mail in pipeline

--- hello_world.groovy
+++ hello_world.groovy
@@ -3,9 +3,8 @@ node {
        echo "Prepare"
     }
     stage('Hello World') {
-        mail to: 'ezhovaelena19@gmail.com',
-            subject: "Hello from '${JOB_NAME}' (${BUILD_NUMBER})",
-            body: "Please go to ${BUILD_URL} and verify the build"
+        echo "Hello from '${JOB_NAME}' (${BUILD_NUMBER})",
+        echo "Please go to ${BUILD_URL} and verify the build"
     }
     stage('Cleanup') {
         echo "Cleanup"
